c4f4f4486c5d3e9673d6d2ca6b06ab0266e21cfe
Alex Morehead <alex.morehead@gmail.com>
2021-11-12 16:05:48 -0600

    Add an example batch prediction script for Alphafold_non_docker (for Slurm HPC systems)

---
+++ batch_run_alphafold.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#SBATCH -N 16
+#SBATCH -t 24:00:00
+#SBATCH --mem 224G
+#SBATCH -J simultaneous-jobsteps
+
+# Define absolute paths for proper script execution
+export MINICONDA_DIR=/home/"$USER"/miniconda3/
+export PROJ_DIR=/home/"$USER"/alpha_non_docker/
+export DATA_DIR=/home/"$USER"/alphafold_databases/
+export OUTPUT_DIR=/home/"$USER"/alphafold_outputs/
+
+# Activate Conda environment for 'alpha_non_docker' project
+source "$MINICONDA_DIR"/bin/activate
+conda activate alphafold_non_docker
+
+# Establish hyperparameters for prediction
+export MAX_TEMPLATE_DATE=2020-05-14
+export USE_GPU=false
+
+# Process each unprocessed FASTA file in the current working directory
+for f in *.fasta;
+  do srun -n1 -N1 -c32 --cpu-bind=cores --exclusive bash "$PROJ_DIR"/run_alphafold.sh -d "$DATA_DIR" -o "$OUTPUT_DIR" -f "$f" -t "$MAX_TEMPLATE_DATE" -g "$USE_GPU" &
+done;
+
+wait
